001de5e55881e30b9e89a1eb872dd542fd1b9100
Yaswanth-modala <rangasrisaiyaswanthmodala@gmail.com>
2026-03-09 14:08:18 +0530

    update

---
+++ routes/productRoute.js
@@ -0,0 +1,7 @@
+import express from "express";
+const productRouter = express.Router();
+
+// Add your product routes here
+// Example: productRouter.get('/', (req, res) => { res.send('Products'); });
+
+export { productRouter };
